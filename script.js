f3684d9f9d225f50c11ddba7015ba0eb2554b963
Viniket <sviniket1201@gmail.com>
2021-06-20 17:06:02 +0530

    final commit

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 function interestInput(val) {
-    document.getElementById('textInput').value=val + "%"; 
-  }
- 
+  document.getElementById('textInput').value=val + "%"; 
+}
+
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var r = document.getElementById('textInput').value;
-    var n = document.querySelector(".year").value;
+  var p = document.getElementById("principal").value;
+  var r = document.getElementById('textInput').value;
+  var n = document.querySelector(".year").value;
+  if(p<=0){
+      alert("Enter a positive number")
+      document.getElementById("principal").value = ""
+  }else{
 
-  
-    let interest = (parseFloat(p)*parseFloat(r)*parseInt(n))/100;
-    // alert(interest);
-    let answer = document.querySelector(".answer");
-    let year = 2020 + parseInt(n)
-    answer.innerHTML = `<p>If you deposit <span style="background-color: yellow;">${p}.</span></p><p> at an interest rate of <span style="background-color: yellow;">${r}.</span></p><p> You will receive an amount of <span style="background-color: yellow;">${interest}.</span></p><p>in the year <span style="background-color: yellow;">${year}.</span></p> `
-}
+  let interest = (parseFloat(p)*parseFloat(r)*parseInt(n))/100;
+  // alert(interest);
+  let answer = document.querySelector(".answer");
+  let year = new Date().getFullyear() + parseInt(n)
+  answer.innerHTML = `<p>If you deposit <span style="background-color: yellow;">${p}.</span></p><p> at an interest rate of <span style="background-color: yellow;">${r}.</span></p><p> You will receive an amount of <span style="background-color: yellow;">${interest}.</span></p><p>in the year <span style="background-color: yellow;">${year}.</span></p> `
+ }
+
+} 
 
         
