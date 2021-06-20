ba1afa5161b35ecde2c7fa165d3c25b3f6e57c9f
Viniket <sviniket1201@gmail.com>
2021-06-20 19:59:06 +0530

    last edited commit

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-function interestInput(val) {
-  document.getElementById('textInput').value=val + "%"; 
+function onChange(v){
+  selrate.innerHTML = v + "%";
+}
+function compute() {
+  let p = Number(document.getElementById("principal").value);
+  let myRate = Number(document.getElementById("myRange").value);
+  let years = Number(document.getElementById("years").value);
+  if (p < 1) {
+      alert("Enter a positive number");
+document.getElementById("principal").value="";
+  }
+  else {
+      let receiveAmount = ((myRate * p * years) / 100).toFixed(2);
+      showintr.innerHTML = `<p>If you deposit <span style="background-color:yellow;">${p}</span>,</p><p>at an Interest rate of <span style="background-color:yellow;">${myRate}%</span>.</p><p>You will recevie an amount of <span style="background-color:yellow;">${receiveAmount}</span>,</p><p>in the year <span style="background-color:yellow;">${new Date().getFullYear() + years}</span>.</p>`
+  }
 }
 
-function compute()
-{
-  var p = document.getElementById("principal").value;
-  var r = document.getElementById('textInput').value;
-  var n = document.querySelector(".year").value;
-  if(p<=0){
-      alert("Enter a positive number")
-      document.getElementById("principal").value = ""
-  }else{
-
-  let interest = (parseFloat(p)*parseFloat(r)*parseInt(n))/100;
-  // alert(interest);
-  let answer = document.querySelector(".answer");
-  let year = new Date().getFullyear() + parseInt(n)
-  answer.innerHTML = `<p>If you deposit <span style="background-color: yellow;">${p}.</span></p><p> at an interest rate of <span style="background-color: yellow;">${r}.</span></p><p> You will receive an amount of <span style="background-color: yellow;">${interest}.</span></p><p>in the year <span style="background-color: yellow;">${year}.</span></p> `
- }
-
-} 
 
         
