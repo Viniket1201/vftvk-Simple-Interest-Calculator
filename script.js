25da240f860bc868230e6760a7cb1765d8f8b696
Viniket <sviniket1201@gmail.com>
2021-06-20 14:20:15 +0530

    updated all files

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function interestInput(val) {
+    document.getElementById('textInput').value=val + "%"; 
+  }
+ 
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById('textInput').value;
+    var n = document.querySelector(".year").value;
+
+  
+    let interest = (parseFloat(p)*parseFloat(r)*parseInt(n))/100;
+    // alert(interest);
+    let answer = document.querySelector(".answer");
+    let year = 2020 + parseInt(n)
+    answer.innerHTML = `<p>If you deposit <span style="background-color: yellow;">${p}.</span></p><p> at an interest rate of <span style="background-color: yellow;">${r}.</span></p><p> You will receive an amount of <span style="background-color: yellow;">${interest}.</span></p><p>in the year <span style="background-color: yellow;">${year}.</span></p> `
 }
+
         
